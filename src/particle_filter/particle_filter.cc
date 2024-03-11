308e9b2be9fd643df2e4ec583842bb9501fdd462
Daniel <dmeza@utexas.edu>
2024-03-10 22:46:34 -0500

    Updated k5 and k6 based on parameter_sweep

--- src/particle_filter/particle_filter.cc
+++ src/particle_filter/particle_filter.cc
@@ -93,8 +93,8 @@ DEFINE_double(k1, 0.5, "Error in translation from translation motion");
 DEFINE_double(k2, 0.3, "Error in rotation from translation motion");
 DEFINE_double(k3, 0.1, "Error in rotation from rotation motion");
 DEFINE_double(k4, 0.1, "Error in translation from rotation motion");
-DEFINE_double(k5, 0.3, "Error in translation from translation motion along major axis");
-DEFINE_double(k6, 0.3, "Error in translation from translation motion along minor axis");
+DEFINE_double(k5, 0.5, "Error in translation from translation motion along major axis");
+DEFINE_double(k6, 0.5, "Error in translation from translation motion along minor axis");
 
 // . fixed
 DEFINE_double(pi, 3.1415926, "Pi");
