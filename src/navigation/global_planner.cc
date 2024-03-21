d655730eb1b116fc1d5a9a63ef4552e3ec9741dc
Daniel <dmeza@utexas.edu>
2024-03-21 17:13:21 -0500

    Continued progress on global planner

--- src/navigation/global_planner.cc
+++ src/navigation/global_planner.cc
@@ -6,44 +6,117 @@
 */
 //========================================================================
 
-#include "global_planner.h"
+#include "global_planner.h"`
 
 namespace global_planner {
 
-  Global_Planner::Global_Planner() :
-    goal_(0, 0),
-    goal_reached_(false),
-    max_nodes_(1000000) {
-      node_map_.clear();
-    }
-
-  void Global_Planner::SetRobotPose(Eigen::Vector2f loc, float angle) {
-    // Create starting node at the robot location
-    Node node;
-    node.id = 0;
-    node.parent = 0;  // Parent is itself, starting node
-    node.loc = loc;
-    node.cost = 0;
-    node.neighbors = new std::vector<Node>();
-
-    // Add to map of nodes
-    node_map_.insert({node.id, node});
+Global_Planner::Global_Planner() :
+  goal_(0, 0),
+  goal_threshold_(0.1),
+  goal_reached_(false),
+  max_nodes_(1000000),
+  graph_resolution_(0.25) {
+    // TODO map_ is already loaded by navigation, adjust so we simply use the same one!
+    // map_.Load(map_file);
+    node_map_.clear();
   }
 
-  void Global_Planner::CalculatePath(Eigen::Vector2f loc, float angle) {
-    // Set new goal
-    goal_ = loc;
+void Global_Planner::SetRobotPose(Eigen::Vector2f loc, float angle) {
+  // Create starting node at the robot location
+  Node node;
+  node.id = 0;
+  node.parent = 0;  // Parent is itself, starting node
+  node.loc = loc;
+  node.cost = 0;
+  node.neighbors = new std::vector<Node>();
 
-    // Loop performing RRT* path planning algorithm
-    unsigned int counter = 0;   // Exit condition if goal is unreachable
-    Node current_node = node_map_.at(0);
-    while (counter < max_nodes_) {
-      // TODO
+  // Add to map of nodes
+  node_map_.insert({node.id, node});
+}
 
+void Global_Planner::CalculatePath(Eigen::Vector2f loc, float angle) {
+  // Set new goal
+  goal_ = loc;
 
-      counter++;
-    }
+  // Loop performing RRT* path planning algorithm
+  counter_ = 0;   // Exit condition if goal is unreachable
+  while (counter_ < max_nodes_) {
+    // Sample a random point in the state space
+    Eigen::Vector2f sampled_loc = SamplePoint();
 
+    // Find closest node to sampled point
+    Node closest_node = FindClosestNode(sampled_loc);
+
+    // Project closest node in the direction of the sampled point and create a new node
+    Node new_node = CreateChildNode(closest_node, sampled_loc);
+
+    // If possible, optimize path
+    // TODO
+
+    counter_++;
   }
+
+}
+
+Eigen::Vector2f Global_Planner::SamplePoint() {
+  // TODO Select a random point in the map.
+  // Ensure it is at least "graph resolution" away from any existing node. Otherwise select a different point.
+  // This has the effect of selecting points in "unexplored" areas.
+
+  Eigen::Vector2f point(0, 0);
+
+  return point;
+}
+
+Node Global_Planner::FindClosestNode(Eigen::Vector2f loc) {
+  float min_dist = 1000;
+  Node closest_node = node_map_.at(0);
+
+  // Loop through node map to find closest node that does not require passing through an obstacle to connect
+  for (auto const& [id, node] : node_map_) {
+    // Check for a smaller distance
+    float dist = (node.loc - loc).norm();
+
+    if (dist < min_dist) {
+      // Check that it does not pass through an obstacle
+      line2f line(
+        node.loc[0], node.loc[1],
+        loc[0], loc[1]
+      );
+
+      // Loop through map lines checking for instersections
+      for (size_t j = 0; j < map_.lines.size(); ++j) {
+        const line2f map_line = map_.lines[j];
+
+        // Check for instersection
+        Eigen::Vector2f instersection_point;
+        bool intersects = map_line.Intersection(ray, &intersection_point);
+
+        if (!intersects) {
+          min_dist = dist;
+          closest_node = node;
+        }
+      }
+    }
+  }
+
+  return closest_node;
+}
+
+Node Global_Planner::CreateChildNode(Node parent, Eigen::Vector2f loc) {
+  // TODO
+  // Calculate direction of projection from parent to point
+  Eigen::Vector2f vect = (loc - parent.loc);
+  float x = parent.loc[0] + (graph_resolution_ / vect.norm() * vect[0]);
+  float y = parent.loc[1] + (graph_resolution_ / vect.norm() * vect[1]);
+
+  // Create new node
+  Node node;
+  node.id = 0;
+  node.parent = parent.id;  // Parent is itself, starting node
+  node.loc = parent.loc + Eigen::Vector2f(x, y);
+  node.cost = 0;
+  node.neighbors = new std::vector<Node>();
+}
 
 }
--- src/navigation/global_planner.h
+++ src/navigation/global_planner.h
@@ -12,6 +12,10 @@
 #include <map>
 #include <eigen3/Eigen/Dense>
 
+#include "shared/math/geometry.h"
+
+using geometry::line2f;
+
 namespace global_planner {
 
 struct Node {
@@ -31,10 +35,25 @@ class Global_Planner {
     void CalculatePath(Eigen::Vector2f loc, float angle);
 
   private:
-    Eigen::Vector2f goal_;     // Goal location (x,y)
-    bool goal_reached_;        // Goal reached flag
-    unsigned int max_nodes_;   // Maximum number of nodes to search
+    Eigen::Vector2f SamplePoint();
 
+    Node Global_Planner::FindClosestNode(Eigen::Vector2f loc);
+
+    Node CreateChildNode(Node parent, Eigen::Vector2f loc);
+
+    // Map of the environment.
+    vector_map::VectorMap map_;
+
+    unsigned int counter_;
+
+    Eigen::Vector2f goal_;     // Goal location (x,y)
+    float goal_threshold_;     // Set distance from goal for success
+    bool goal_reached_;        // Goal reached flag
+
+    float m_ = 3;
+
+    float graph_resolution_;    // minimum distance between nodes
+    unsigned int max_nodes_;   // Maximum number of nodes to search
     std::map<unsigned int, Node> node_map_;   // Map of nodes
 };
 
